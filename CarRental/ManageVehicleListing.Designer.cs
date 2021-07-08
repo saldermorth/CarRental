3046a9af642034470fb2d27e7b625d33364f79a6
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-07-08 08:18:26 +0200

    Update ManageVehicleListing.Designer.cs
    # new buttons

--- CarRental/ManageVehicleListing.Designer.cs
+++ CarRental/ManageVehicleListing.Designer.cs
@@ -31,6 +31,9 @@ namespace CarRental
         {
             this.gvVehicleList = new System.Windows.Forms.DataGridView();
             this.CarRentalLable = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gvVehicleList)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +42,7 @@ namespace CarRental
             this.gvVehicleList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gvVehicleList.Location = new System.Drawing.Point(12, 116);
             this.gvVehicleList.Name = "gvVehicleList";
-            this.gvVehicleList.Size = new System.Drawing.Size(775, 247);
+            this.gvVehicleList.Size = new System.Drawing.Size(251, 195);
             this.gvVehicleList.TabIndex = 0;
             // 
             // CarRentalLable
@@ -52,11 +55,41 @@ namespace CarRental
             this.CarRentalLable.TabIndex = 1;
             this.CarRentalLable.Text = "Manage Vehicle Listing";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(336, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(336, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // ManageVehicleListing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.CarRentalLable);
             this.Controls.Add(this.gvVehicleList);
             this.Name = "ManageVehicleListing";
@@ -72,5 +105,8 @@ namespace CarRental
 
         private System.Windows.Forms.DataGridView gvVehicleList;
         private System.Windows.Forms.Label CarRentalLable;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
